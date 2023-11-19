e3be34ed8ce37d216d6de3217b698149314e8f10
hangyalpeter <hangyal.peter@gmail.com>
2023-11-19 19:32:36 +0100

    Add best times score to levels and show on level select

--- Assets/Scripts/Level Finish/LevelFinish.cs
+++ Assets/Scripts/Level Finish/LevelFinish.cs
@@ -1,52 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class LevelFinish : MonoBehaviour
 {
     private bool isRunning = true;
+    float elapsedTime = 0f;
     [SerializeField] private TMPro.TextMeshProUGUI timerText;
 
     [SerializeField] private GameObject endLevelPanel;
     [SerializeField] private AudioSource levelFinishSound;
-    Coroutine timerCoroutine;
-    private Stopwatch stopwatch;
     
     void Start()
     {
-        stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        timerCoroutine = StartCoroutine(UpdateTimer());
+        Debug.Log("best time: " + PlayerPrefs.GetFloat(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + "_BestCompletionTime"));
     }
 
-    IEnumerator UpdateTimer()
+    void Update()
     {
-        while (isRunning)
+        if (isRunning)
         {
-            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
-            int minutes = (int)(elapsedMilliseconds / 60000);
-            int seconds = (int)((elapsedMilliseconds / 1000) % 60);
-            int milliseconds = (int)(elapsedMilliseconds % 1000);
+            elapsedTime += Time.deltaTime;
+            int minutes = (int)(elapsedTime / 60);
+            int seconds = (int)(elapsedTime % 60);
+            int milliseconds = (int)((elapsedTime * 1000) % 1000);
             timerText.text = string.Format("Time: {0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
-            yield return null;
+
         }
     }
 
-    public void StopTimer()
+    private void SaveBestCompletionTime()
     {
-        isRunning = false;
-        stopwatch.Stop();
-        StopCoroutine(timerCoroutine);
+        var currentLevelName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        if (PlayerPrefs.HasKey(currentLevelName + "_BestCompletionTime"))
+        {
+            var bestCompletionTime = PlayerPrefs.GetFloat(currentLevelName + "_BestCompletionTime");
+            if (elapsedTime < bestCompletionTime)
+            {
+                PlayerPrefs.SetFloat(currentLevelName + "_BestCompletionTime", elapsedTime);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(currentLevelName + "_BestCompletionTime", elapsedTime);
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player_Child")
         {
             levelFinishSound.Play();
             endLevelPanel.SetActive(true);
-            StopTimer();
+            isRunning = false;
+            SaveBestCompletionTime();
             Time.timeScale = 0f;
         }
 
--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
     [SerializeField] private GameObject levelSelectPanel;
     [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] public TextMeshProUGUI? bestTimeText;
+    [SerializeField] public TextMeshProUGUI? buttonLabel;
+
+    private void Start()
+    {
+        var bestTime = PlayerPrefs.GetFloat(buttonLabel.text + "_BestCompletionTime");
+        
+        int minutes = (int)(bestTime / 60);
+        int seconds = (int)(bestTime % 60);
+        int milliseconds = (int)((bestTime * 1000) % 1000);
+        bestTimeText.text = string.Format("Time: {0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
     public void Level1()
     {
         levelSelectPanel.SetActive(false);
@@ -21,11 +34,11 @@ public class LevelSelect : MonoBehaviour
     public void Back()
     {
         levelSelectPanel.SetActive(false);
-        mainMenuPanel.SetActive(true);
+        //mainMenuPanel.SetActive(true);
     }
    public void OnLevelSelect()
     {
-        mainMenuPanel.SetActive(false);
+        //mainMenuPanel.SetActive(false);
         levelSelectPanel.SetActive(true);
     }
 
